462f130843f3cc0fde61bba48dec77d2089efe3a
yang69 <yang66999@qq.com>
2017-09-29 13:53:18 +0800

    242. Valid Anagram【java】判断变位词（异序词）

---
+++ algorithms/java/ValidAnagram.java
@@ -0,0 +1,49 @@
+/**
+ * Created by Yang on 2017/9/29.
+ ************************************************************************************************
+ * 242. Valid Anagram
+ * https://leetcode.com/problems/valid-anagram/
+ ************************************************************************************************
+ * Given two strings s and t, write a function to determine if t is an anagram of s.
+ *
+ * Example 1:
+ *  Input:  s = "anagram", t = "nagaram"
+ *  Output: true
+ *
+ * Example 2:
+ *  Input:  s = "rat", t = "car"
+ *  Output: false
+ *
+ * Note:
+ *      You may assume the string contains only lowercase alphabets.
+ * Follow up:
+ *      What if the inputs contain unicode characters? How would you adapt your solution to
+ *      such case?
+ *      --- Use a HashMap
+ ************************************************************************************************
+ */
+public class ValidAnagram {
+    public boolean isAnagram(String s, String t) {
+        if( s.length() != t.length() ) {
+            return false;
+        }
+        int[] counts = new int[26];
+        for(int i = 0; i < s.length(); i++) {
+            counts[s.charAt(i) - 'a']++;
+            counts[t.charAt(i) - 'a']--;
+        }
+        for(int count : counts) {
+            if(count != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        ValidAnagram validAnagram = new ValidAnagram();
+
+        System.out.println(validAnagram.isAnagram("anagram", "nagaram") + " <---> true");
+        System.out.println(validAnagram.isAnagram("rat", "car") + " <---> false");
+    }
+}
